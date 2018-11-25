e86e3a4c0e55ef3ef423eb4b73ee66dcc655443e
junichi-hatano <junichi-hatano@hands-lab.com>
2018-11-26 00:25:43 +0900

    add forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { CounterModule } from './counter/counter.module';
 import { StoreModule } from '@ngrx/store';
@@ -8,15 +8,20 @@ import { reducers, metaReducers } from './state';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     CounterModule,
     StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : []
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/counter/counter.module.ts
+++ src/app/counter/counter.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CounterComponent } from './counter.component';
 import { StoreModule } from '@ngrx/store';
 import * as fromCounter from './state';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    StoreModule.forFeature('counter', fromCounter.reducers, { metaReducers: fromCounter.metaReducers })
+    StoreModule.forFeature('counter', fromCounter.reducers, { metaReducers: fromCounter.metaReducers }),
+    FormsModule,
+    ReactiveFormsModule,
   ],
   declarations: [CounterComponent],
   exports: [CounterComponent]
